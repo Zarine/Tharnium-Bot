b79b317731400a21334a6c6eb77ca34ef6624ce1
Zarine <nicommki@hotmail.fr>
2019-01-22 00:47:33 +0200

    Heroku change

--- bot.js
+++ bot.js
@@ -13,6 +13,19 @@ var jdr = require('./jdr');
 var commandHelp = require('./commandHelp');
 var guessGame = require('./guessGame');
 
+// Heroku stuff
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Heroku stuff end
+
 process.setMaxListeners(500);
 
 // Configure logger settings
