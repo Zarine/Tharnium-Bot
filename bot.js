b93392ac82ea647d7aa56dd2bec5ccb4960cf9ee
Zarine <nicommki@hotmail.fr>
2019-01-30 14:38:29 +0100

    clear heroku stuff toward worker

--- bot.js
+++ bot.js
@@ -18,13 +18,6 @@ var guessGame = require('./guessGame');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 // Heroku stuff end
 
 process.setMaxListeners(500);
@@ -70,12 +63,3 @@ bot.on('message', (message) =>
 });
 
 bot.login(process.env.auth);
-
-function working()
-{
-  http.get('https://tharnium.herokuapp.com/', (res) => {
-    logger.info("Call to self done");
-  });
-}
-
-setInterval(working, 300000);
