67ef98f68f3becf3a765b3d3a9cb07ba064aeee1
Zarine <nicommki@hotmail.fr>
2020-10-29 12:14:16 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ module.exports = {
   getRole: function(guild, roleName)
   {
     var roleToFind;
-    guild.roles.forEach(function(role) {
+    guild.roles.cache.forEach(function(role) {
       if(role.name === roleName) { roleToFind = role; }
     });
     return roleToFind;
